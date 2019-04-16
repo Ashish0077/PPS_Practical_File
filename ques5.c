e85fb91bd6c5bc615daa3d97a20f21cc31a69ad3
Ashish Arora <ashish007722@gmail.com>
2019-04-16 22:57:19 +0000

    Create ques1.c
    WAP that swaps values of two variables using a third variable.
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques5.c
@@ -0,0 +1,27 @@
+/*QUES-5 : WAP that swaps values of two variables using a third variable. */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b; //Declaration of Variables
+
+    //Taking Input
+	printf("Enter Values : ");
+	scanf("%d%d", &a, &b);
+
+	//Printing Orignal Values
+	system("clear");
+	printf("Orignal Values\na : %d\nb = %d \n", a, b);
+
+	/* Swapping the Variables */
+    int temp = a;
+    a = b;
+    b = temp;
+
+    //Printing After Swapping
+	printf("\nValues Swapped\na : %d\nb = %d \n", a, b);
+
+	return 0;
+}
