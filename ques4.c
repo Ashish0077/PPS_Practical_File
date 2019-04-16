6bfd822caeff7a488eb0829ccb8b9b9156478965
Ashish Arora <ashish007722@gmail.com>
2019-04-17 03:15:06 +0530

    Update ques4.c
    WAP that accepts the temperature in Centigrade and converts into Fahrenheit using the formula
    C/5=(F-32)/9.
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- ques4.c
+++ ques4.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	float c; //Declaration of Variables
 
-    //Taking Input
+    	//Taking Input
 	printf("Enter the Temperature(°C) : ");
 	scanf("%f", &c);
 
 	/* Converting into Fahrenheit */
-    float f = (float) (9*(c/5)+32);
+    	float f = (float) (9*(c/5)+32);
 
 
-    //Printing SI & CI
+    	//Printing  Temperature in Fahrenheit
 	printf("Fahrenheit : %.3f °F\n", f);
 
 	return 0;
