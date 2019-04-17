fbd4eb50888c420aaa4f1d06c887eac8b2e954ff
Ashish Arora <ashish007722@gmail.com>
2019-04-17 21:36:23 +0000

    Create ques9.c
    WAP that tells whether a given year is a leap year or not.
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques9.c
@@ -0,0 +1,21 @@
+/*QUES-9 : WAP that tells whether a given year is a leap year or not. */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int year; //Declaration of Variables
+
+    //Taking Input
+	printf("Enter Year : ");
+	scanf("%d", &year);
+
+	//Checking if the year is Leap yr or not
+	if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	    printf("%d is a Leap Year\n", year);
+	else
+	    printf("%d is not a Leap year\n", year);
+
+	return 0;
+}
