70ad546ca4b4a66c14d9db9807356352e92c2288
Ashish Arora <ashish007722@gmail.com>
2019-04-17 22:50:40 +0000

    Create ques12.c
    WAP to print the sum of all numbers up to a given number.
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques12.c
@@ -0,0 +1,25 @@
+/*QUES-1 :  WAP to print the sum of all numbers up to a given number. */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+    //Declaration of Variables
+	int n, sum = 0;
+
+	//Taking Input
+	printf("Enter N : ");
+	scanf("%d", &n);
+
+    //Performing Sum of all Numbers till n
+    for(int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    //Printing Result
+	printf("SUM : %d", sum);
+
+	return 0;
+}
