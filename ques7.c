4128cb6541fbb2c2d568412b27bba0bfdd00990c
Ashish Arora <ashish007722@gmail.com>
2019-04-17 20:50:36 +0000

    Create ques7.c
    WAP to find the greatest of three numbers
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques7.c
@@ -0,0 +1,21 @@
+/*QUES-7 : WAP to find the greatest of three numbers */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b, c; //Declaration of Variables
+
+    //Taking Input
+	printf("Enter Values : ");
+	scanf("%d%d%d", &a, &b, &c);
+
+	//Checking Equality
+	int max = (a > b ? (a > c ? a : c) : (b > c ? b : c));
+
+	//output
+	printf("Greatest Number is : %d\n", max);
+
+	return 0;
+}
