bab41464ed3449f936aabbc72a27987d8613441f
Ashish Arora <ashish007722@gmail.com>
2019-04-17 20:56:49 +0000

    Create ques8.c
    WAP that finds whether a given number is even or odd.
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques8.c
@@ -0,0 +1,21 @@
+/*QUES-8 : WAP that finds whether a given number is even or odd. */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int a; //Declaration of Variables
+
+    //Taking Input
+	printf("Enter a Number : ");
+	scanf("%d", &a);
+
+	//Checking if the number is Even or Odd
+	if(a % 2 == 0)
+	    printf("%d is an Even Number\n", a);
+	else
+	    printf("%d is and Odd Number\n", a);
+
+	return 0;
+}
