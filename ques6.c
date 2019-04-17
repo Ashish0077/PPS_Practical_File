1f16d94a81f8fcc7f3ca5f850f5485bea00f6796
Ashish Arora <ashish007722@gmail.com>
2019-04-17 20:32:24 +0000

    Create ques6.c
    WAP that checks whether the two numbers entered by the user are equal or not

---
+++ ques6.c
@@ -0,0 +1,21 @@
+/*QUES-5 :  WAP that checks whether the two numbers entered by the user are equal or not */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b; //Declaration of Variables
+
+    //Taking Input
+	printf("Enter Values : ");
+	scanf("%d%d", &a, &b);
+
+	//Checking Equality
+	if(a == b)
+	    printf("Both the Numbers are Equal\n");
+    else
+        printf("Not Equal\n");
+
+	return 0;
+}
