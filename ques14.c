0acdbc1edbe0629234b4bfbe774f4431f0e9b426
Ashish Arora <ashish007722@gmail.com>
2019-04-17 23:19:27 +0000

    Create ques14.c
    WAP to print sum of even and odd numbers from 1 to N numbers
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques14.c
@@ -0,0 +1,29 @@
+/*QUES-14 : WAP to print sum of even and odd numbers from 1 to N numbers */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main(void)
+{
+    //Declaration of Variables
+	int n, esum = 0, osum = 0;
+
+	//Taking Input
+	printf("Enter N : ");
+	scanf("%d", &n);
+
+    //Performing Sum of even and odd numbers from 1 to N numbers
+    for(int i = 0; i <= n; i++)
+    {
+        if(i % 2 == 0)
+            esum += i;
+        else
+            osum += i;
+    }
+
+    //Printing Result
+	printf("Sum of All Even Numbers : %d \nSum of All Odd Numbers : %d \n", esum, osum);
+
+	return 0;
+}
