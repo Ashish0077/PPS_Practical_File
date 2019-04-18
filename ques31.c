de68a25c00c41232cab03f410e66c0f480b3a315
Ashish Arora <ashish007722@gmail.com>
2019-04-18 11:15:46 +0000

    Create ques31.c
    WAP to compare the contents of two files and determine whether they are same or not.
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques31.c
@@ -0,0 +1,54 @@
+/* Ques31 - WAP to compare the contents of two files and determine whether they are same or not.  */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    FILE *file1, *file2;  //Declaration of file Pointer
+
+    file1 = fopen("new.txt", "r");
+
+    file2 = fopen("new1.txt", "r");
+
+    if(!file1 || !file2)
+    {
+        printf("404 File Not Found.\n");
+        exit(1);
+    }
+
+    //Calculating the Size of file 1
+    int size1 = 0;
+    while(!feof(file1))
+    {
+        size1++;
+        fgetc(file1);
+    }
+
+    //Calculating the Size of file 2
+    int size2 = 0;
+    while(!feof(file2))
+    {
+        size2++;
+        fgetc(file2);
+    }
+
+    if(size1 == size2)
+    {
+        int flag = 1;
+        while(!feof(file1))
+        {
+            if( fgetc(file1) != fgetc(file2) )
+            {
+                printf("Not Equal\n");
+                exit(2);
+            }
+        }
+        if(flag == 1)
+            printf("Both the Files are Same\n");
+    }
+    else
+        printf("Files are Not Same\n");
+
+    return 0;
+}
