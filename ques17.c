6ea04eaa3ad48232ad9303aa99c159eb6d6a64a6
Ashish Arora <ashish007722@gmail.com>
2019-04-18 00:18:21 +0000

    Create ques17.c
    WAP to find the sum of digits of the entered number.
    
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ ques17.c
@@ -0,0 +1,27 @@
+/*QUES-17 : WAP to find the sum of digits of the entered number. */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+    //Declaration of Variables
+	int n, sum = 0, r;
+
+	//Taking Input
+	printf("Enter N : ");
+	scanf("%d", &n);
+
+    //Calculating the sum of
+    while(n > 0)
+    {
+        r = n % 10;
+        n = n / 10;
+        sum += r;
+    }
+
+    //printing results
+    printf("SUM : %d\n", sum);
+
+	return 0;
+}
